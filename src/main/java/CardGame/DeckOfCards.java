f32a1faf4396115dc4a7aa437f3de58193dcb381
Alexander <alexaga@stud.ntnu.no>
2021-03-17 20:00:03 +0100

    Created the DeckOfCards constructor which creates the deck of cards.

--- src/main/java/CardGame/DeckOfCards.java
+++ src/main/java/CardGame/DeckOfCards.java
@@ -1,5 +1,21 @@
 package CardGame;
 
+import java.util.ArrayList;
+
 public class DeckOfCards {
+    private final char[] suit = { 'S', 'H', 'D', 'C' };
+    private ArrayList<PlayingCard> DeckOfCards;
+
+    public DeckOfCards() {
+        DeckOfCards = new ArrayList<>();
+
+        for (char suit: suit) {
+            for(int i = 0; i >13; i++) {
+                DeckOfCards.add(new PlayingCard(suit,i));
+            }
+        }
+    }
+
+
 
 }
